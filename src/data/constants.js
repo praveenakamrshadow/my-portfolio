6520e610c76a40cbebce71a949da69479161c18b
Praveen Sahu <praveensahu.work@gmail.com>
2024-04-13 07:51:36 +0000

    added few projects

--- src/data/constants.js
+++ src/data/constants.js
@@ -259,6 +259,50 @@ export const projects = [
     },
     {
         id: 5,
+        title: 'CineWave',
+        description:
+            'This is a web application built with React that allows users to explore and discover movies and TV shows. It utilizes various libraries and tools to provide a seamless user experience.',
+        image: 'https://github.com/praveenakamrshadow/images/blob/main/CineWave.png?raw=true',
+        tags: ['React Js', 'API','Axios','TailwindCSS', 'React Player', 'Redux'],
+        category: 'frontend',
+        github: 'https://github.com/praveenakamrshadow/CineWave',
+        webapp: 'https://cinewave-react.vercel.app/',
+    },
+    {
+        id: 6,
+        title: 'AxoApe',
+        description:
+            '"Super App" is a React-based web application designed for a seamless user experience. It encompasses features such as user registration, category selection, weather updates, news feeds, notes management, timers, and entertainment browsing using external APIs. The development process is marked by structured milestones, with progress documented through commits to a GitHub repository. The goal is to deliver a comprehensive and responsive application.',
+        image: 'https://github.com/praveenakamrshadow/images/blob/main/axo-ape.png?raw=true',
+        tags: ['React Js', 'Framer Motion', 'TailwindCSS', 'GSAP'],
+        category: 'frontend',
+        github: 'https://github.com/praveenakamrshadow/axo-ape',
+        webapp: 'https://axo-ape.vercel.app/',
+    },
+    {
+        id: 7,
+        title: 'GuardianVault',
+        description:
+            '"Super App" is a React-based web application designed for a seamless user experience. It encompasses features such as user registration, category selection, weather updates, news feeds, notes management, timers, and entertainment browsing using external APIs. The development process is marked by structured milestones, with progress documented through commits to a GitHub repository. The goal is to deliver a comprehensive and responsive application.',
+        image: 'https://github.com/praveenakamrshadow/images/blob/main/GuardianVault.png?raw=true',
+        tags: ['React Js', 'JavaScript', 'TailwindCSS'],
+        category: 'frontend',
+        github: 'https://github.com/praveenakamrshadow/GuardianVault',
+        webapp: 'https://guardian-vault.vercel.app/',
+    },
+    {
+        id: 8,
+        title: 'PokeDex',
+        description:
+            '"Super App" is a React-based web application designed for a seamless user experience. It encompasses features such as user registration, category selection, weather updates, news feeds, notes management, timers, and entertainment browsing using external APIs. The development process is marked by structured milestones, with progress documented through commits to a GitHub repository. The goal is to deliver a comprehensive and responsive application.',
+        image: 'https://github.com/praveenakamrshadow/images/blob/main/PokeDex.png?raw=true',
+        tags: ['React Js', 'Axios', 'API', 'CSS'],
+        category: 'frontend',
+        github: 'https://github.com/praveenakamrshadow/PokeDex',
+        webapp: 'https://pokedex-react-praveen.vercel.app/',
+    },
+    {
+        id: 7,
         title: 'Working on Backend projects deploying soon....',
         image: 'https://images.unsplash.com/photo-1553451166-232112bda6f6?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         tags: ['In Progress'],
@@ -267,7 +311,7 @@ export const projects = [
         webapp: '#',
     },
     {
-        id: 6,
+        id: 8,
         title: 'Working on Fullstack projects deploying soon....',
         image: 'https://media.istockphoto.com/id/508408464/vector/work-in-progress-loading-bar.jpg?s=612x612&w=0&k=20&c=NyDSPinMdT1wuEODQQPk2YS2Tt-qf3K-w620zK3F9ls=',
         tags: ['In Progress'],
