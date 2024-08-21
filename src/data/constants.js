31e0dd226fbcddf4630effc8c0faa3fa41e6e14f
praveen.pkyt327867 <praveen.pkyt327867@gmail.com>
2024-08-21 10:07:49 +0000

    added git and figma icons

--- src/data/constants.js
+++ src/data/constants.js
@@ -87,7 +87,7 @@ export const skills = [
         skills: [
             {
                 name: 'Git',
-                image: 'https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667',
+                image: '/assets/git.svg',
             },
             {
                 name: 'GitHub',
@@ -107,7 +107,7 @@ export const skills = [
             },
             {
                 name: 'Figma',
-                image: 'https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667',
+                image: '/assets/figma.svg',
             },
         ],
     },
